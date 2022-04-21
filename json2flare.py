3e2f79d5bba48944de75c51ea2fa72d124eca178
Julian Loiacono <jcloiacon@gmail.com>
2022-04-21 17:40:26 -0400

    additions and fixes

--- json2flare.py
+++ json2flare.py
@@ -2,22 +2,27 @@ import sys
 import json
 infile = sys.argv[1]
 with open(infile, 'r') as f:
-    indict = json.loads(f.read())
+	indict = json.loads(f.read())
 
 	
-def json2bs(name, indict):
+def json2flare(name, indict):
 	children = []
 	for k, v in indict.items():
 		if type(v) is dict:
-			if len(v.items()):
-				newchild = json2bs(k, v)
+			if k.endswith(".json"):
+				with open(k, 'r') as f:
+					newchild = json2flare(k.replace(".json",""), json.loads(f.read()))
+			elif len(v.items()):
+				newchild = json2flare(k, v)
 			else:
-				newchild = {"name":k, "size":200}
+				newchild = {"name":k, "value":200}
+		else:
+			newchild = {"name":k, "value":200}
 		children += [newchild]
 	
 	newdict = {}
-	newdict["children"] = children
 	newdict["name"] = name
+	newdict["children"] = children
 	return newdict
 		
-print(json.dumps(json2bs(infile, indict), indent = 3))
+print(json.dumps(json2flare(infile, indict), indent = 3))
